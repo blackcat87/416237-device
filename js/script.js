e06fc902398c102c73a3786b9cf9f69a19dfd1d9
Victoria <v.9252766408@yandex.ru>
2017-09-07 21:31:00 +0300

    На проверку

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
 var button = document.querySelector(".contacts-btn");
 var modalContact = document.querySelector(".modal-contact");
 var closeContact = document.querySelector(".modal-contact .modal-close");
+var nameUser = modalContact.querySelector("[name=name-user]");
+var emailUser = modalContact.querySelector("[name=user-email]");
 var closeMap = document.querySelector(".modal-map .modal-close");
 var modalMap = document.querySelector(".modal-map");
 var buttonMap = document.querySelector(".map");
+var form = document.querySelector("form");
+
+var storage = localStorage.getItem("name-user");
 
 if (button) {
   button.addEventListener("click", function (event) {
     event.preventDefault();
     modalContact.classList.add("modal-show");
+    nameUser.focus();
+    if (storage) {
+      nameUser.value = storage;
+      emailUser.focus();
+    } else {
+      nameUser.focus();
+    }
   });
 }
+
+
+form.addEventListener("submit", function (event) {
+  if (!nameUser.value || !emailUser.value) {
+    event.preventDefault();
+    modalContact.classList.add("modal-error");
+  } else {
+    localStorage.setItem("nameUser", nameUser.value);
+  }
+});
+
 
 if (closeContact) {
   closeContact.addEventListener("click", function (event) {
@@ -32,3 +55,11 @@ if (closeMap) {
     modalMap.classList.remove("modal-show");
   });
 }
+
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode === 27) {
+    if (modalContact.classList.contains("modal-show")) {
+      modalContact.classList.remove("modal-show");
+    }
+  }
+});
