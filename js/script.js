bd5627447ac8e210cef9bea9983907aadece24b6
Victoria <v.9252766408@yandex.ru>
2017-09-11 11:22:36 +0300

    На проверку №3

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 var button = document.querySelector(".contacts-btn");
 var modalContact = document.querySelector(".modal-contact");
 var closeContact = document.querySelector(".modal-contact .modal-close");
-var nameUser = modalContact.querySelector("[name=name_user]");
-var emailUser = modalContact.querySelector("[name=user_email]");
-var message = modalContact.querySelector("[name=user_message]");
+var nameUser = document.querySelector("[name=name_user]");
+var emailUser = document.querySelector("[name=user_email]");
+var message = document.querySelector("[name=user_message]");
 var closeMap = document.querySelector(".modal-map .modal-close");
 var modalMap = document.querySelector(".modal-map");
 var buttonMap = document.querySelector(".map");
 var form = document.querySelector(".contact-form");
 var catalog = document.querySelector(".category");
 
-catalog.addEventListener("focus", function (event){
+catalog.addEventListener("focus", function (event) {
   catalog.classList.add("focus-within");
 
 }, true);
 
-catalog.addEventListener("blur", function (event){
+catalog.addEventListener("blur", function (event) {
   catalog.classList.remove("focus-within");
 }, true);
 
@@ -27,20 +27,22 @@ if (button) {
   });
 }
 
-form.addEventListener("submit", function (event) {
-  if (!nameUser.value) {
-    event.preventDefault();
-    nameUser.classList.add("modal-error");
-  }
-  if (!emailUser.value) {
-    event.preventDefault();
-    emailUser.classList.add("modal-error");
-  }
-  if (!message.value) {
-    event.preventDefault();
-    message.classList.add("modal-error");
-  }
-});
+if (form) {
+  form.addEventListener("submit", function (event) {
+    if (!nameUser.value) {
+      event.preventDefault();
+      nameUser.classList.add("modal-error");
+    }
+    if (!emailUser.value) {
+      event.preventDefault();
+      emailUser.classList.add("modal-error");
+    }
+    if (!message.value) {
+      event.preventDefault();
+      message.classList.add("modal-error");
+    }
+  })
+};
 
 if (closeContact) {
   closeContact.addEventListener("click", function (event) {
